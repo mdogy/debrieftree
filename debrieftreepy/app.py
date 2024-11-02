444be2bb59150045dbb6428b0a7b507a7a4df479
Michael Grossberg <mgrossberg@ccny.cuny.edu>
2024-11-02 23:13:51 +0900

    Added the a real csv file, modified the code to work with it. It now outputs debug statements to a log file, has a debug level that can be turned on and off, has a data file checker to indicate if the data file is in the proper format.

--- debrieftreepy/app.py
+++ debrieftreepy/app.py
@@ -7,10 +7,11 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from loguru import logger
-from tqdm import tqdm
+#from tqdm import tqdm
 import yaml
 
 
+LOGGER_FILE = "debug.log"
 MAXLOOP = 1000
 
 def display_question(data_loaded, 
@@ -28,36 +29,48 @@ def display_question(data_loaded,
         Optional[str]: The final answer based on the user's selections, if any.
     """
 
-  
     # Base case: if the tree is a string, return it as the final answer
     assert len(current_path) > 0
+    logger.debug("current_path: {current_path}")
     assert all(one_id in list(data_loaded.iloc[:,0]) for one_id in current_path)
- 
+    
     for _ in range(MAXLOOP):
         current_node = current_path[-1]
-        print("current_node: ", current_node)
+        logger.debug(f"current_node: {current_node}")
         text_input = None
-        node_row = data_loaded.iloc[current_node,:]
+        node_row = data_loaded[data_loaded["nodeID"] == current_node].iloc[0]
+        logger.debug(f"node_row: {node_row}")
         node_type, node_text = node_row["nodeType"], node_row["nodeText"]
-        select_children = data_loaded["parentID"] == current_node
+        logger.debug(f"node_type: {node_type}")
+        logger.debug(f"node_text: {node_text}")
+        logger.debug(f"type(node_type): {type(node_type)}")
+        select_children = (data_loaded["parentID"] == current_node)
         children_rows = data_loaded[select_children]
+        logger.debug(f"children_rows: {children_rows}")
+        
         # Remove empty values
         node_dict = {key:value 
                         for key, value in node_row.to_dict().items()
                         if str(value) not in ["", np.nan ,".nan", "nan", float("nan")]}
+        logger.debug(f"node_dict: {node_dict}")
         # Remove keys that are not needed for the report
         for key in ["nodeType", "nodeID", "parentID", "option"]:
             if key in node_dict:
                 del node_dict[key]
-                
-        # Dump the node_dict to a yaml string as report
-        if node_type == "leaf":
+        logger.debug(f"node_dict: {node_dict}")   
+        logger.debug(f"node_type: {node_type}")
+
+        
+        if node_type == 'leaf':
+            # Dump the node_dict to a yaml string as report
+            logger.debug(f"Branch Leaf")
+            
             formatted_node = yaml.dump(node_dict,
                                       default_flow_style=False)
             collected_info.append(formatted_node)
             return formatted_node
         elif node_type == "selectbox":
-          
+            logger.debug(f"Branch Selectbox")
             option_list = list(children_rows["option"])
             if len(option_list) == 0:
               Exception(f"Node {current_node} has no options")
@@ -85,8 +98,12 @@ def display_question(data_loaded,
                 return None
             node_dict["response"] = text_input
             # There can be only one child node for a text area
-            next_node = children_rows['nodeID'].values[0]
+            next_children_rows = children_rows['nodeID']
+            logger.debug(f"next_children_rows: {next_children_rows}")
+            next_node = next_children_rows.values[0]
+            logger.debug(f"next_node: {next_node}")
             current_path.append(next_node)
+            logger.debug(f"current_path: {current_path}")
         else: 
             question_type = data_loaded.loc[current_node,"nodeType" ]
             Exception(f"Question type: {question_type} not implemented")
@@ -95,38 +112,116 @@ def display_question(data_loaded,
         if node_dict:
             formatted_node = yaml.dump(node_dict,
                                 default_flow_style=False)
-            collected_info.append(formatted_node)                      
-def main():
+            collected_info.append(formatted_node)
+            
+def check_df(df, ROOTNODEID):
+    check_passed = True
+    if 'nodeID' not in df.columns:
+        logger.error("nodeID column is missing")
+        check_passed = False
+        return check_passed
     
-  DATAPATH = Path("data/raw")
-  FNAME = "SampleFormatTreeData.csv"
-  df = pd.read_csv(DATAPATH / FNAME)
-  ROOTNODEID = 0
-  
-  if 'path' not in st.session_state:
-      st.session_state.path = []
-  
-  st.session_state.path.clear()
-  st.session_state.path.append(0)
-  
-  if 'collected_info' not in st.session_state:
-      st.session_state.collected_info = []
-      
-  st.session_state.collected_info.clear()
-  
-  final_answer = display_question(df, 
-                                  st.session_state.path,
-                                  st.session_state.collected_info)
+    if 'nodeType' not in df.columns:
+        logger.error("nodeType column is missing")
+        check_passed = False
+        return check_passed
+    
+    if 'nodeText' not in df.columns:
+        logger.error("nodeText column is missing")
+        check_passed = False
+        return check_passed
+    
+    if 'parentID' not in df.columns:
+        logger.error("parentID column is missing")
+        check_passed = False
+        return check_passed
+    
+    # Check that the parentID is in the nodeID
+    for index, row in df.iterrows():
+        if row['nodeID']!= ROOTNODEID:
+            if row['parentID'] not in df['nodeID'].to_list():
+                logger.error(f"parentID: {row['parentID']} is not in the nodeID")
+                check_passed = False
+        
+    # Check that the nodeID is unique
+    if len(df['nodeID'].unique()) != len(df['nodeID']):
+        logger.error("nodeID is not unique")
+        check_passed = False
+        return check_passed
+    
+    # Check that the if the nodeType is textarea, then there is only one child
+    for index, row in df.iterrows():
+        if row['nodeType'] == 'textarea':
+            if len(df[df['parentID'] == row['nodeID']]) > 1:
+                logger.error(f"nodeID: {row['nodeID']} has more than one child")
+                check_passed = False
+    
+    # Check that if the nodeType is leaf, then there are no children
+    for index, row in df.iterrows():
+        if row['nodeType'] == 'leaf':
+            if len(df[df['parentID'] == row['nodeID']]) > 0:
+                logger.error(f"nodeID: {row['nodeID']} has children")
+                check_passed = False
+    
+    # Check that if the nodeType is selectbox, then there are children
+    for index, row in df.iterrows():
+        if row['nodeType'] == 'selectbox':
+            if len(df[df['parentID'] == row['nodeID']]) == 0:
+                logger.error(f"nodeID: {row['nodeID']} has no children")
+                check_passed = False
+    
+    # Check that if the nodeID has no children, then the nodeType is leaf
+    for index, row in df.iterrows():
+        if len(df[df['parentID'] == row['nodeID']]) == 0:
+            if row['nodeType'] != 'leaf':
+                logger.error(f"nodeID: {row['nodeID']} has no children but nodeType is not leaf")
+                check_passed = False
+    
+   
+    return check_passed 
+                
+def main():
 
-  if final_answer:  
-    if st.session_state.collected_info:
-      st.write("### Information Collected:")
-      st.text('\n\n'.join(st.session_state.collected_info))
-      print("Information Collected: ", st.session_state.collected_info)
-  else:
-    print("Still Collecting Information")
-  
-  return
+    DATAPATH = Path("data/raw")
+    FNAME = "SampleFormatTreeData.csv"
+    FNAME = "DecisionTrees3.csv"
+
+    fid = open(LOGGER_FILE, "a")
+    logger.add(fid,
+                format="{time} {level} {message}",
+                level="DEBUG")
+
+    df = pd.read_csv(DATAPATH / FNAME)
+    
+    ROOTNODEID = min(df['nodeID'].to_list())
+     
+    if not check_df(df, ROOTNODEID):
+        raise Exception("Dataframe check failed")
+    
+
+    if 'path' not in st.session_state:
+        st.session_state.path = []
+    
+    st.session_state.path.clear()
+    st.session_state.path.append(ROOTNODEID)
+    
+    if 'collected_info' not in st.session_state:
+        st.session_state.collected_info = []
+        
+    st.session_state.collected_info.clear()
+    final_answer = display_question(df, 
+                                    st.session_state.path,
+                                    st.session_state.collected_info)
+
+    if final_answer:  
+        if st.session_state.collected_info:
+            st.write("### Information Collected:")
+            st.text('\n\n'.join(st.session_state.collected_info))
+            logger.info(f"Information Collected: {st.session_state.collected_info}")
+    else:
+        logger.info(f"Still Collecting Information")
+    
+    return
   
     
 if __name__ == "__main__":
